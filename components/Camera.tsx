3ad10d83f0f3469a04165f87556fb562d334e152
Filip <filipkujundzic3@gmail.com>
2024-06-28 14:25:25 +0200

    Added torch to the camera

--- components/Camera.tsx
+++ components/Camera.tsx
@@ -1,15 +1,28 @@
 import { CameraView, useCameraPermissions } from "expo-camera";
-import { Pressable, Button, StyleSheet, Text, View } from "react-native";
+import {
+    Pressable,
+    Button,
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+} from "react-native";
 import { useState } from "react";
 import { COLORS } from "../utils/constants";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 const Camera = () => {
     const [permission, requestPermission] = useCameraPermissions();
     const [scanned, setScanned] = useState(false);
+    const [torchEnabled, setTorchEnabled] = useState(false);
 
     if (!permission) {
         return <View />;
     }
+
+    const toggleTorch = () => {
+        setTorchEnabled((prev) => !prev);
+    };
 
     if (!permission.granted) {
         return (
@@ -35,17 +48,26 @@ const Camera = () => {
                     barcodeTypes: ["qr"],
                 }}
                 onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
+                enableTorch={torchEnabled}
             >
                 <View style={styles.overlay} />
             </CameraView>
-            {scanned && (
-                <Pressable
-                    onPress={() => setScanned(false)}
-                    style={styles.scan_again}
-                >
-                    <Text style={styles.text}>Scan again</Text>
-                </Pressable>
-            )}
+
+            <TouchableOpacity onPress={toggleTorch} style={styles.bulb}>
+                {torchEnabled ? (
+                    <MaterialCommunityIcons
+                        name="lightbulb-off"
+                        size={26}
+                        color="#fff"
+                    />
+                ) : (
+                    <MaterialCommunityIcons
+                        name="lightbulb"
+                        size={26}
+                        color="#fff"
+                    />
+                )}
+            </TouchableOpacity>
         </View>
     );
 };
@@ -70,7 +92,7 @@ const styles = StyleSheet.create({
         borderColor: "rgba(255, 255, 255, 0.8)",
         backgroundColor: "rgba(0, 0, 0, 0.2)",
     },
-    scan_again: {
+    bulb: {
         position: "absolute",
         bottom: 30,
         right: 10,
